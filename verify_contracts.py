cde4657b6f898237b1d6df0a26df74eece20397a
jorfermo <jorfermo@gmail.com>
2018-10-29 10:18:46 +0000

    Add script to verify contracts

--- verify_contracts.py
+++ verify_contracts.py
@@ -5,6 +5,8 @@ import argparse
 import os
 import colorlog
 import inspect
+import hashlib
+from eospy.cleos import Cleos
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
@@ -16,12 +18,17 @@ parser.add_argument("-d", '--debug', action="store_true",
                     dest="debug", help='Print debug info')
 parser.add_argument('-l', '--log_file', default='{}.log'.format(
     os.path.basename(__file__).split('.')[0]), help='Log file')
-args = parser.parse_args()
+parser.add_argument('-u', '--api_endpoint',
+                    default='http://127.0.0.1:8888', help='EOSIO API endpoint URI')
+parser.add_argument('-c', '--contracts_path',
+                    default='/opt/telos-launch/source/telos/build/contracts/', help='Path of the compiled contracts')
 
+args = parser.parse_args()
 VERBOSE = args.verbose
 DEBUG = args.debug
 LOG_FILE = args.log_file
-
+API_ENDPOINT = args.api_endpoint
+CONTRACTS_PATH = args.contracts_path
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -56,12 +63,27 @@ CONTRACTS = [
   }
 ]
 
-cleos = Cleos(url='http://api.pennstation.eosnewyork.io:7001')
+cleos = Cleos(url=API_ENDPOINT)
+def sha256sum(filename):
+    h = hashlib.sha256()
+    with open(filename, 'rb', buffering=0) as f:
+        for b in iter(lambda: f.read(128*1024), b''):
+            h.update(b)
+    return h.hexdigest()
 
 def main():
   for contract in CONTRACTS:
-    logger.info('Verifying {} contract'.format(contract['contract']))
-
+    try:
+      chain_hash = cleos.get_code(contract['account'])['code_hash']
+      contract_hash = sha256sum('{}{}/{}.wasm'.format(CONTRACTS_PATH, contract['contract'], contract['contract']))
+    
+      if chain_hash == contract_hash:
+        logger.info('Contract {} for account {} matches'.format(contract['contract'], contract['account']))
+      else:
+        logger.critical('Contract {} for account {} doesn\'t match'.format(contract['contract'], contract['account']))
+    except Exception as e:
+      logger.critical('Error checking contract: {}'.format(e))
+      quit()
 
 if __name__ == "__main__":
     main()
